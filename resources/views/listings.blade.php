ac181bcf44495a83de0dce6fe3c0bc5f252810d3
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-09-23 22:15:04 +0300

    Layout and sections

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,21 +1,29 @@
-<h1>{{ $heading }} </h1>
+@extends('layout')
+{{-- Layout is header and footer, everything but the main view 
+    yield('content')
+    section('content')
+    later I will try to make the layout a component
+    so it will be easier than extends
+    --}}
+
+@section('content')
+    <h1>{{ $heading }} </h1>
 
 
-@unless (count($listings) == 0)
-
-    @foreach ($listings as $listing)
-        {{-- Directive start with @ --}}
-        <h2>
-            <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }} </a>
-        </h2>
-        <p>
-            {{ $listing['description'] }}
-        </p>
-    @endforeach
-@else
-    <p>No listings found</p>
-@endunless
-
+    @unless (count($listings) == 0)
+        @foreach ($listings as $listing)
+            {{-- Directive start with @ --}}
+            <h2>
+                <a href="/listings/{{ $listing['id'] }}">{{ $listing['title'] }} </a>
+            </h2>
+            <p>
+                {{ $listing['description'] }}
+            </p>
+        @endforeach
+    @else
+        <p>No listings found</p>
+    @endunless
+@endsection
 
 
 {{--
