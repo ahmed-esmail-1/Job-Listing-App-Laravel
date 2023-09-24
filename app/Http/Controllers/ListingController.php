d28da2122b9cf2b48c88747cdf9980ff772f445d
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-09-25 00:12:03 +0300

    Tag filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -13,7 +13,9 @@ class ListingController extends Controller
     {
         return view('listings.index', [
             //'heading' => 'Latest Listings',
-            'listings' => Listing::all()      //use all the Listing model methods, this is php syntax
+            //'listings' => Listing::all()      //use all the Listing model methods, this is php syntax
+            'listings' => Listing::latest()-> //sorted by latest (the jobs)
+                filter(request(['tag']))->get()  //silter by tag (ex: vue)
         ]);
     }
 
