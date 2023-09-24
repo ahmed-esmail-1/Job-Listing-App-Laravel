fe15b5ae25dbed0f31aaf5decfbafa997905c96e
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-09-24 19:57:59 +0300

    Changing card wrapper into component too

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,8 +1,8 @@
-@props(['listing'])
+@props(['listing']) <!-- Receive listing -->
 
 {{-- Directive start with @ --}}
 <!-- Item 1 -->
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
         <div>
@@ -29,4 +29,4 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -17,7 +17,7 @@
 
         @unless (count($listings) == 0)
             @foreach ($listings as $listing)
-                <x-listing-card :listing="$listing" />
+                <x-listing-card :listing="$listing" />{{-- Send to the child component --}}
             @endforeach
         @else
             <p>No listings found</p>
