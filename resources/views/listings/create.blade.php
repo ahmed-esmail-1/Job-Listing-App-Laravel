c54a5ae9fea0e51fe55664b3837d14d7ae0828ef
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-10-01 20:34:36 +0300

    Small updates

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -2,9 +2,9 @@
     <x-card class=" p-10 max-w-lg mx-auto mt-24">
         <header class="text-center">
             <h2 class="text-2xl font-bold uppercase mb-1">
-                Create a Gig
+                Create a Job
             </h2>
-            <p class="mb-4">Post a gig to find a developer</p>
+            <p class="mb-4">Post a Job to find a developer</p>
         </header>
 
         <form action="">
@@ -62,7 +62,7 @@
 
             <div class="mb-6">
                 <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-                    Create Gig
+                    Create Job
                 </button>
 
                 <a href="/" class="text-black ml-4"> Back </a>
