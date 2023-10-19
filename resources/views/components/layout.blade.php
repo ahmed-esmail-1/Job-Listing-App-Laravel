5e88039895c0cc17269398bb32f9d4e31b5cbcc7
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-10-19 21:36:02 +0300

    Auth:2. Controller

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,8 @@
     <link rel="icon" href="images/favicon.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
     <script src="//unpkg.com/alpinejs" de fer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
@@ -30,10 +31,10 @@
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
             <li>
-                <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
             <li>
-                <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
         </ul>
