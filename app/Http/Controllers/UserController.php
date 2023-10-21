481c982d9014525c3b4c254324f136c5a49f139e
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-10-21 14:02:35 +0300

    Auth:4.Submit route, Store controller, Hash password, Old data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Validation\Rule; // Add this import statement fot Rule
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,5 +13,26 @@ class UserController extends Controller
     public function create()
     {
         return view('users.register');
+    }
+
+    // Create new user
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6'],
+        ]);
+
+        //Hash password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        //Create user
+        $user = User::create($formFields);
+
+        //Login
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in');
     }
 }
