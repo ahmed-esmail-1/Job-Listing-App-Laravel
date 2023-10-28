55fa74f97131ab8c4b5ec875b8647d6ff28e35a7
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-10-28 15:01:57 +0300

    Auth and guest middleware, Who can post, and others

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : route('login'); //We named our route to login bcs of this
     }
 }
