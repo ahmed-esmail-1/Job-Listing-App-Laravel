f87c898d25572cf800ca84419dbe5efb167408cc
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-11-06 11:11:11 +0300

    Add ownership to listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -61,6 +61,7 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         }
 
+        $formFields['user_id'] = auth()->id();
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
