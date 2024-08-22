9c608c106750fcfbe8c95ce54967b71894cd996b
Ahmed Esmail <ahmedalawi01@gmail.com>
2024-08-23 00:31:58 +0300

    Integrating Filament Admin Panel, Listing Resource

--- README.md
+++ README.md
@@ -11,3 +11,29 @@ Set up localdb, run migrations, then php artisan serve.
 
 
 If you have any issues feel free to reach out.
+
+
+
+Integrating Filament:-
+
+- Installing:-
+composer require filament/filament
+
+- Creating a new panel
+php artisan filament:install --panels
+Id: admin
+
+- Creating a user
+php artisan make:filament-user
+Ahmed
+info@ahmedesmail.tech
+Password
+Success! info@ahmedesmail.tech may now log in at http://localhost/admin/login.
+
+- Creating a new resource (Core concept)
+php artisan make:filament-resource Listing
+
+- For file uploading
+php artisan storage:link
+http://127.0.0.1:8000/storage/01J5XS72YXHTYXD4TGZ2VM085B.svg
+
---
+++ app/Filament/Resources/ListingResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ListingResource\Pages;
+use App\Filament\Resources\ListingResource\RelationManagers;
+use App\Models\Listing;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ListingResource extends Resource
+{
+    protected static ?string $model = Listing::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('user_id')->required(),
+                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\FileUpload::make('logo')->downloadable(),
+                Forms\Components\TextInput::make('tags')->required(),
+                Forms\Components\TextInput::make('company')->required(),
+                Forms\Components\TextInput::make('location')->required(),
+                Forms\Components\TextInput::make('email')->email()->required(),
+                Forms\Components\TextInput::make('website')->required(),
+                Forms\Components\TextInput::make('description')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('user_id'),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\ImageColumn::make('logo'),
+                Tables\Columns\TextColumn::make('tags'),
+                Tables\Columns\TextColumn::make('company'),
+                Tables\Columns\TextColumn::make('location'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('website'),
+                Tables\Columns\TextColumn::make('description'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListListings::route('/'),
+            'create' => Pages\CreateListing::route('/create'),
+            'edit' => Pages\EditListing::route('/{record}/edit'),
+        ];
+    }
+}
